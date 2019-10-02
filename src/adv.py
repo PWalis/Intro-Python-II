559f9adb824b188409d94946de38dc79017c38e8
PWalis <pwilky1016@gmail.com>
2019-10-02 16:58:48 -0700

    Finished MVP for day 1

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+import player
 
 # Declare all the rooms
 
@@ -25,12 +26,16 @@ earlier adventurers. The only exit is to the south."""),
 # Link rooms together
 
 room['outside'].n_to = room['foyer']
+
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
 room['foyer'].e_to = room['narrow']
+
 room['overlook'].s_to = room['foyer']
+
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
+
 room['treasure'].s_to = room['narrow']
 
 #
@@ -38,6 +43,9 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+
+
+player_1 = player.Player('outside')
 
 # Write a loop that:
 #
@@ -49,3 +57,35 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+
+on = 1
+print('ENTER "q" TO QUIT')
+while on == 1:
+    paths = room[player_1.location].available_path()
+    
+    print('Current location: ', player_1.location)
+    print(f'Possible directions: {paths} \n')
+    k = input('Press key: ')
+
+    if k == 'q':
+        break
+    if k in paths:
+        if k == 'n':
+            newloc = room[player_1.location].n_to.room.split(' ',1)[0].lower()
+            player_1.update_location(newloc)
+
+        elif k == 'e':
+            newloc = room[player_1.location].e_to.room.split(' ',1)[0].lower()
+            player_1.update_location(newloc)
+
+        elif k == 's':
+            newloc = room[player_1.location].s_to.room.split(' ',1)[0].lower()
+            player_1.update_location(newloc)
+            
+        elif k == 'w':
+            newloc = room[player_1.location].w_to.room.split(' ',1)[0].lower()
+            player_1.update_location(newloc)
+    else:
+        print('\n###You can not go that way###\n')
+        
