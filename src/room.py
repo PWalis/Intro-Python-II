8fd1a39038c7a989de7085a46639041ad48ad803
PWalis <pwilky1016@gmail.com>
2019-10-02 16:59:25 -0700

    Second commit because VS code is being weird

--- src/room.py
+++ src/room.py
@@ -1,2 +1,22 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+    def __init__(self, room, description):
+        self.room = room
+        self.desc = description
+        self.n_to = None
+        self.w_to = None
+        self.e_to = None
+        self.s_to = None
+
+    def available_path(self):
+        path_list = []
+        if self.n_to != None:
+            path_list.append('n')
+        if self.w_to != None:
+            path_list.append('w')
+        if self.e_to != None:
+            path_list.append('e')
+        if self.s_to != None:
+            path_list.append('s')
+        return path_list
